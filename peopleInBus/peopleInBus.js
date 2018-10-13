19415c41c22196324e0d75fec8edc186cff536a1
Shiv Gogna <sgogna0912@gmail.com>
2018-10-13 08:24:12 -0700

    Setup for new challenge

---
+++ peopleInBus/peopleInBus.js
@@ -0,0 +1,12 @@
+/*
+Number of People in the Bus
+  [People get into the bus, People get out the bus]
+  Create variable to hold amount of people
+  Create a loop to go through main array
+    Target array index by busStop[i][0]
+    Substract busStops[i][0] from busStops[i][1]
+  Return outside loop because we want it to iterate throughout all arrays
+  
+  Question: Why do I get -3 when I try to return without people + busStops[i][0] - busStops[i][1]
+  I think I have the answer, I'd like to tell you what I think it's doing when we speak later today
+*/
