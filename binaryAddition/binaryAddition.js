69124db22257a3052c699b4c637b7d846868ef7b
Shiv Gogna <sgogna0912@gmail.com>
2018-10-13 08:26:04 -0700

    solved binary addition

--- binaryAddition/binaryAddition.js
+++ binaryAddition/binaryAddition.js
@@ -3,3 +3,19 @@ Binary Addition
   First argument of toString is radix which is the root. 
   Binary only can go up to 2, 0 and 1. So by saying toString(2) it turns it into a binary
 */
+
+function addBinary(a,b) {
+    return (a + b).toString(2);
+  }
+  
+  function nArr(n) {
+      let firstArr = [];
+      let poop = 0;
+      for(let i = 0; i < n; i++) {
+          firstArr.push([]);
+          for(let j = 0; j < n; j++){
+              firstArr[i].push(poop += 2);
+          }
+      }
+      console.log(firstArr)
+  }
