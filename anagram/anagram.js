6d685ee556ed1714a22aec665b8d542f28747d35
Shiv Gogna <sgogna0912@gmail.com>
2018-10-20 09:15:39 -0700

    solutions to anagram

--- anagram/anagram.js
+++ anagram/anagram.js
@@ -8,3 +8,48 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
+
+//SOLUTION 1.1
+function anagrams(stringA, stringB) {
+    stringA = stringA.replace(/[^\w]/g, "").toLowerCase().split('').sort().join('');
+    stringB = stringB.replace(/[^\w]/g, "").toLowerCase().split('').sort().join('');
+    return stringA === stringB
+  }
+  
+  //SOLUTION 1.2
+  function anagrams(stringA, stringB) {
+    return cleanString(stringA) === cleanString(stringB);
+  }
+  
+  function cleanString(str) {
+    return str.replace(/[^\w]/g, "").toLowerCase().split('').sort().join('');
+  }
+  
+  //SOLUTION 2
+  function anagrams(stringA, stringB) {
+    let aCharMap = buildCharMap(stringA);
+    let bCharMap = buildCharMap(stringB);
+    
+    if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+      return false;
+    }
+    for(let char in aCharMap) {
+      if(aCharMap[char] !== bCharMap[char]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  function buildCharMap(str) {
+    const charMap = {};
+    
+    for(let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+      charMap[char] = charMap[char] + 1 || 1;
+    }
+    return charMap;
+  }
+  
+  
+  module.exports = anagrams;
+  
