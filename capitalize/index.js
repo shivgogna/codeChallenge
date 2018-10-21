ef3ece8a690fc771a447149ec00e1885cccf0f7e
Shiv Gogna <sgogna0912@gmail.com>
2018-10-20 23:06:19 -0700

    capitalize using traditional for loop

--- capitalize/index.js
+++ capitalize/index.js
@@ -6,3 +6,13 @@
 //   capitalize('a short sentence') --> 'A Short Sentence'
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+//SOLUTION 1
+function capitalize(str) {
+    str = str.split(' ');
+    let sent = [];
+    for(let i = 0; i < str.length; i++) {
+      sent.push(str[i][0].toUpperCase() + str[i].slice(1));
+    }
+    return sent.join(' ');
+  }
