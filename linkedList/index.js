c7b0f43fbcf0c45fcd331e1db91bd8ba375d7f67
Shiv Gogna <sgogna0912@gmail.com>
2018-10-23 16:49:28 -0700

    getFirst and getLast method created

--- linkedList/index.js
+++ linkedList/index.js
@@ -25,6 +25,23 @@ class LinkedList {
         return counter;
     }
 
+    getFirst() {
+        return this.head;
+    }
+
+    getLast() {
+        if (!this.head) {
+            return null;
+        }
+
+        let node = this.head;
+        while (node) {
+            if (!node.next) {
+                return node;
+            }
+            node = node.next;
+        }
+    }
 
     
 }
