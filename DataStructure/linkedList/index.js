20fbaaa15dd1f814d79108531c4aa542f88db2b5
Shivinder Gogna <sgogna0912@gmail.com>
2018-10-31 11:30:41 -0700

    Methods
    Completed all methods and tested

--- DataStructure/linkedList/index.js
+++ DataStructure/linkedList/index.js
@@ -51,16 +51,63 @@ class LinkedList {
   }
 
   removeLast() {
-    if (!this.head) return null;
-    let node = this.head;
+    if (!this.head) return;
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+    let previous = this.head;
+    let node = this.head.next;
 
-    while (node) {
-      if (!node.next) {
-        node = null;
-      }
+    while (node.next) {
+      previous = node;
       node = node.next;
+    }
+    previous.next = null;
+  }
+
+  insertLast(data) {
+    const last = this.getLast();
+    if (last) {
+      last.next = new Node(data);
+    } else {
+      this.head = new Node(data);
     }
   }
 
-  insertLast() {}
+  getAt(index) {
+    let counter = 0;
+    let node = this.head;
+
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+      counter++;
+      node = node.next;
+    }
+    return null;
+  }
+
+  removeAt(index) {
+    if (!this.head) return;
+    if (index === 0) this.head = this.head.next;
+    const previous = this.getAt(index - 1);
+    if (!previous || !previous.next) return;
+    previous.next = previous.next.next;
+  }
+
+  insertAt(data, index) {
+    if (!this.head) {
+      this.head = new Node(data);
+      return;
+    }
+    if (index === 0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
+    const previous = this.getAt(index - 1) || this.getLast();
+    const node = new Node(data, previous.next);
+    previous.next = node;
+  }
 }
