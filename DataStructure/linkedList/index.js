4d7d65b58376cff8936ea24ef2c113740bf86bf6
Shivinder Gogna <sgogna0912@gmail.com>
2018-10-31 10:23:31 -0700

    LinkedList
    Format document and redoing all linked list methods

--- DataStructure/linkedList/index.js
+++ DataStructure/linkedList/index.js
@@ -13,4 +13,54 @@ class LinkedList {
   insertFirst(data) {
     this.head = new Node(data);
   }
+
+  size(integer) {
+    let counter = 0;
+    let node = this.head;
+
+    while (node) {
+      counter++;
+      node = node.next;
+    }
+    return counter;
+  }
+
+  getFirst(Node) {
+    return this.head;
+  }
+
+  getLast(Node) {
+    if (!this.head) return null;
+    let node = this.head;
+
+    while (node) {
+      if (!node.next) {
+        return node;
+      }
+      node = node.next;
+    }
+  }
+
+  clear() {
+    let node = null;
+  }
+
+  removeFirst() {
+    if (!this.head) return;
+    this.head = this.head.next;
+  }
+
+  removeLast() {
+    if (!this.head) return null;
+    let node = this.head;
+
+    while (node) {
+      if (!node.next) {
+        node = null;
+      }
+      node = node.next;
+    }
+  }
+
+  insertLast() {}
 }
